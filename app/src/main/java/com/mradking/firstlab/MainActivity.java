f087f0a78cc979138ed9045cfcfd001000e48bb1
pawandeepzin <mradking4l7@gmail.com>
2023-03-16 06:48:13 +0530

    My First Lab

---
+++ app/src/main/java/com/mradking/firstlab/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mradking.firstlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
